7504a3f4ed283cbb714b33bdb0c72cfc59b8047d
James-Livesey <jamesliveg@gmail.com>
2025-05-14 18:39:29 +0100

    Don't have override for `print` function as it is already handled by `CATTO_LOG`

--- src/contexts.h
+++ src/contexts.h
@@ -555,31 +555,29 @@ void catto_run(catto_Context* context) {
     while (catto_step(context)) {}
 }
 
-#ifndef CATTO_CUSTOM_PRINT_COMMAND
-    void catto_command_print(catto_Context* context) {
-        catto_Bool appendFlag = CATTO_FALSE;
+void catto_command_print(catto_Context* context) {
+    catto_Bool appendFlag = CATTO_FALSE;
 
-        while (catto_hasNextArg(context)) {
-            catto_AstNode* arg = catto_getNextArg(context);
-            catto_Char* string = catto_asString(catto_evalExpression(context, arg));
+    while (catto_hasNextArg(context)) {
+        catto_AstNode* arg = catto_getNextArg(context);
+        catto_Char* string = catto_asString(catto_evalExpression(context, arg));
 
-            if (!catto_hasNextArg(context) && catto_hasAppendFlag(arg)) {
-                appendFlag = CATTO_TRUE;
-            }
-
-            CATTO_LOG(string);
-            CATTO_FREE(string);
-
-            if (catto_hasNextArg(context)) {
-                CATTO_LOG(" ");
-            }
+        if (!catto_hasNextArg(context) && catto_hasAppendFlag(arg)) {
+            appendFlag = CATTO_TRUE;
         }
 
-        if (!appendFlag) {
-            CATTO_LOG("\n");
+        CATTO_LOG(string);
+        CATTO_FREE(string);
+
+        if (catto_hasNextArg(context)) {
+            CATTO_LOG(" ");
         }
     }
-#endif
+
+    if (!appendFlag) {
+        CATTO_LOG("\n");
+    }
+}
 
 void catto_command_goto(catto_Context* context) {
     catto_Int lineNumber = (catto_Int)catto_asNumber(catto_evalNextArg(context));
@@ -1109,10 +1107,7 @@ catto_TypedValue catto_function_upper(catto_Context* context, catto_DataType ret
 }
 
 void catto_addContextStandardCommands(catto_Context* context) {
-    #ifndef CATTO_CUSTOM_PRINT_COMMAND
-        catto_addCommand(context, "print", &catto_command_print);
-    #endif
-
+    catto_addCommand(context, "print", &catto_command_print);
     catto_addCommand(context, "goto", &catto_command_goto);
     catto_addCommand(context, "gosub", &catto_command_gosub);
     catto_addCommand(context, "return", &catto_command_return);
